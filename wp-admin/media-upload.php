5a8a59106a03b1671e5a630940a1aa7d4285de0b
thoughtsofstone <88274076+thoughtsofstone@users.noreply.github.com>
2021-12-14 02:18:09 -0500

    update 2021-1214

---
+++ wp-admin/media-upload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://thoughtsofstone.github.io/wp-login.php?redirect_to=https%3A%2F%2Fwww.thoughtsofstone.com%2Fwp-admin%2Fmedia-upload.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://thoughtsofstone.github.io/wp-login.php?redirect_to=https%3A%2F%2Fwww.thoughtsofstone.com%2Fwp-admin%2Fmedia-upload.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://thoughtsofstone.github.io/wp-login.php?redirect_to=https%3A%2F%2Fwww.thoughtsofstone.com%2Fwp-admin%2Fmedia-upload.php&reauth=1">https://thoughtsofstone.github.io/wp-login.php?redirect_to=https%3A%2F%2Fwww.thoughtsofstone.com%2Fwp-admin%2Fmedia-upload.php&reauth=1</a></p>
+	</body>
+</html>
