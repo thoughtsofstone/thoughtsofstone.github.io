abd03db89dbbe8e1a9de0a4b8e956f3d0e8b0760
thoughtsofstone <88274076+thoughtsofstone@users.noreply.github.com>
2022-01-31 18:22:31 -0500

    update 31 jan 2022

--- wp-admin/media-upload.php
+++ wp-admin/media-upload.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=https://thoughtsofstone.github.io/wp-login.php?redirect_to=http%3A%2F%2Fbox5257.temp.domains%2F%7Ehoughty5%2Fwp-admin%2Fmedia-upload.php&reauth=1">
+		<meta http-equiv="refresh" content="0;url=https://thoughtsofstone.github.io/wp-login.php?redirect_to=https%3A%2F%2Fthoughtsofstone.com%2Fwp-admin%2Fmedia-upload.php&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "https://thoughtsofstone.github.io/wp-login.php?redirect_to=http%3A%2F%2Fbox5257.temp.domains%2F%7Ehoughty5%2Fwp-admin%2Fmedia-upload.php&reauth=1";
+			window.location = "https://thoughtsofstone.github.io/wp-login.php?redirect_to=https%3A%2F%2Fthoughtsofstone.com%2Fwp-admin%2Fmedia-upload.php&reauth=1";
 		</script>
 
-		<p>You are being redirected to <a href="https://thoughtsofstone.github.io/wp-login.php?redirect_to=http%3A%2F%2Fbox5257.temp.domains%2F%7Ehoughty5%2Fwp-admin%2Fmedia-upload.php&reauth=1">https://thoughtsofstone.github.io/wp-login.php?redirect_to=http%3A%2F%2Fbox5257.temp.domains%2F%7Ehoughty5%2Fwp-admin%2Fmedia-upload.php&reauth=1</a></p>
+		<p>You are being redirected to <a href="https://thoughtsofstone.github.io/wp-login.php?redirect_to=https%3A%2F%2Fthoughtsofstone.com%2Fwp-admin%2Fmedia-upload.php&reauth=1">https://thoughtsofstone.github.io/wp-login.php?redirect_to=https%3A%2F%2Fthoughtsofstone.com%2Fwp-admin%2Fmedia-upload.php&reauth=1</a></p>
 	</body>
 </html>
